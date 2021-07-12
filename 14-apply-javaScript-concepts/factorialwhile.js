bb618c84f4572af1ac2102b27f07432ef2f240fd
naeemme <abun1347@gmail.com>
2021-07-12 23:48:05 +0600

    apply-js

---
+++ 14-apply-javaScript-concepts/factorialwhile.js
@@ -0,0 +1,14 @@
+function factorial (n){
+    var factorial = 1;
+
+    var i=1;
+    var factorial = 1;
+    while(i<=n){
+    factorial = factorial*i;
+    i++;
+    }
+
+    return factorial;    
+}
+    var result = factorial(6);
+    console.log(result);
---
+++ 14-apply-javaScript-concepts/fibonacci.js
@@ -0,0 +1,11 @@
+function fibonacci(n){
+
+
+var fibo = [0,1];
+for (var i = 2; i<= n; i++){
+    fibo[i]=fibo[i-1] + fibo[i-2];
+}
+    return fibo;
+}
+var result = fibonacci(15);
+console.log(result);
